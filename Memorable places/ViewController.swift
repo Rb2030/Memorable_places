cbb8eaaeedaa400e7514a799c2f0954a99e2ef08
Rb2030 <rossmail2050@gmail.com>
2017-08-06 16:42:17 +0100

    first commit

---
+++ Memorable places/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Memorable places
+//
+//  Created by Ross on 06/08/2017.
+//  Copyright © 2017 Braaaaap. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if activePlace != -1 {
+            
+            //Get place details to display on map
+            
+            if places.count > activePlace {
+                
+                if let name = places[activePlace]["name"] {
+                    
+                    if let lat = places[activePlace]["lat"] {
+                        
+                        if let lon = places[activePlace]["lon"] {
+                            
+                            if let latitude = Double(lat) {
+                                
+                                if let longitude = Double(lon) {
+                                
+                                let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                    
+                                    let coordinate  = CLLocationCoordinate2D(latitude: latitude,longitude: longitude)
+                                    
+                                    let region = MKCoordinateRegion(center: coordinate, span: span)
+                                    
+                                    self.map.setRegion(region, animated: true)
+                                    
+                                    let annotation = MKPointAnnotation()
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        print(activePlace)
+    }
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//
+}
+}
+
